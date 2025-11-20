a546dc9341bebd9f9eef9ba14633a05c48841730
bugsagweler <bugsagweler@gmail.com>
2025-11-20 15:58:29 +0200

    JavaScript issue

--- main.js
+++ main.js
@@ -47,25 +47,26 @@ loginForm.addEventListener("submit", function(event) {
 let toggleNavStatus = false;
 
 let toggleNav =function(){
-    let getSidebar = document.querySelector(".nav-sidebar");
-    let getSidebarUl = document.querySelector(".nav-sidebar ul");
-    let getSidebarTitle = document.querySelector(".nav-sidebar span");
-    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+    const getSidebar = document.querySelector(".nav-sidebar");
+    const getSidebarUl = document.querySelector(".nav-sidebar ul");
+    const getSidebarTitle = document.querySelector(".nav-sidebar span");
+    const getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
 
     if (toggleNavStatus === false){
       getSidebarUl.style.visibility = "visible";
       getSidebar.style.width = "272px";
-      gitSidebarTitle.style.opacity = "0.5";
+      getSidebarTitle.style.opacity = "0.5";
 
       let arrayLength = getSidebarLinks.length;
       for(let i = 0; i < arrayLength; i++){
         getSidebarLinks[i].style.opacity = "1";
       }
+
       
       toggleNavStatus = true;
     } else if(toggleNavStatus === true){
         getSidebar.style.width = "50px";
-        gitSidebarTitle.style.opacity = "0.5";
+        getSidebarTitle.style.opacity = "0.5";
         
         let arrayLength = getSidebarLinks.length;
         for(let i = 0; i < arrayLength; i++){
