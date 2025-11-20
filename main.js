332c7c775d6ec200e3d507d329120546daa3aaee
bugsagweler <bugsagweler@gmail.com>
2025-11-20 15:41:52 +0200

    JavaScript

--- main.js
+++ main.js
@@ -53,16 +53,27 @@ let toggleNav =function(){
     let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
 
     if (toggleNavStatus === false){
-      getSidebarUl.style.visisbility = "visible";
+      getSidebarUl.style.visibility = "visible";
       getSidebar.style.width = "272px";
       gitSidebarTitle.style.opacity = "0.5";
 
       let arrayLength = getSidebarLinks.length;
-      for(let i = 0; i < arrayLength.length; i++){
+      for(let i = 0; i < arrayLength; i++){
         getSidebarLinks[i].style.opacity = "1";
       }
       
       toggleNavStatus = true;
+    } else if(toggleNavStatus === true){
+        getSidebar.style.width = "50px";
+        gitSidebarTitle.style.opacity = "0.5";
+        
+        let arrayLength = getSidebarLinks.length;
+        for(let i = 0; i < arrayLength; i++){
+            getSidebarLinks[i].style.opacity = "0";
+        }
+        
+        getSidebarUl.style.visibility = "hidden";
 
+        toggleNavStatus = false;
     }
 }
